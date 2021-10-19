187fcc11f6ba4fea5836e0453c83f653d8a3c534
Zajith Corro <zajithcorrov@hotmail.com>
2021-10-18 20:53:53 -0500

    :lipstick: Alert when the cart is empty
    Alert when the cart doesn't have products, colors and styles

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 export default class Cart {
   constructor() {
     this._products = [];
+    this._updateCart();
   }
 
   addToCart(product, quantity) {
@@ -30,5 +31,15 @@ export default class Cart {
       quantity: oldProduct.quantity + quantity,
     };
     this._products[productIndex] = newProduct;
+  }
+
+  _updateCart() {
+    const cartList = document.querySelector('.cart-list');
+
+    if (!this._products.length) {
+      cartList.innerHTML = `<div class="cart-list-empty">
+        Your cart is empty. Add products from the list
+      </div>`;
+    }
   }
 }
